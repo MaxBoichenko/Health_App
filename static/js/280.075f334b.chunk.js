1c75af5ce5a6ff40d5d9f08f9f71bfb8f5c379d8
MaxBoichenko <97298946+MaxBoichenko@users.noreply.github.com>
2023-02-13 16:26:21 +0000

    Deploying to gh-pages from @ MaxBoichenko/Health_App@fd9ee2241140ddd47e344be1c9c625d99b20cb1b 🚀

---
+++ static/js/280.075f334b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[280],{7280:function(e,t,r){r.r(t);var a=r(3329);t.default=function(){return(0,a.jsx)("div",{children:"\u041d\u0438\u0447\u0435\u0433\u043e \u043d\u0435 \u043d\u0430\u0439\u0434\u0435\u043d\u043e !"})}}}]);
+//# sourceMappingURL=280.075f334b.chunk.js.map
