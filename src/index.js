fae88d7c3d8f82f068d3793876c9b20fdcc098ba
DmytroNadolynskyi <113470234+DmytroNadolynskyi@users.noreply.github.com>
2023-02-15 15:51:11 +0200

    home-edit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-
 import { persistor, store } from 'redux/store';
-import { App } from 'components/App';
 
-import Loader from 'components/Loader/Loader';
-// import './styles/styles.scss';
+import Loader from 'components/Loader';
+import './styles/styles.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
